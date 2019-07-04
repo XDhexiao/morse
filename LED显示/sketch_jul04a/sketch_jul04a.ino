b22bdb86c4e15f42d7e8ab9bb0fa7d7bb150728a
XDhexiao <52460836+XDhexiao@users.noreply.github.com>
2019-07-04 15:51:12 +0800

    Add files via upload

---
+++ LED显示/sketch_jul04a/sketch_jul04a.ino
@@ -0,0 +1,98 @@
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  Serial.begin(9600);
+ 
+}
+int income=0;
+void loop()
+{
+ if(Serial.available()>0)
+ {
+   income=Serial.read();
+   Serial.println(income,DEC);
+   digitalWrite(7, LOW);
+   switch(income)
+  {
+    case 48:
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    break;
+   case 49:
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    break;
+   
+   case 50:
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    break;
+   
+   case 51:
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    break;
+   
+   case 52:
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+    break;
+   
+   case 53:
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+    break;
+   
+   case 54:
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+    break;
+   
+   case 55:
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+    break;
+   
+   case 56:
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, HIGH);
+    break;
+   
+   case 57:
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, HIGH);
+    break;
+   default:
+    break;
+  }
+  delay(10);
+   digitalWrite(7, HIGH);
+   delay(10);
+ }
+}
+
+   
